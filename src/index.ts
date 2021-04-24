db864a5f391843582213c8312c760f0cb86cd064
appcoreopc <jeremy@jeremys-MacBook-Pro.local>
2021-04-25 06:24:10 +0800

    added trade categories

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,39 @@
 import { createServer } from "http";
 import express from 'express';
 import { Server } from 'socket.io';
+import { emit } from "node:process";
 
 const app = express();
-
 const server = createServer(app);
-
 const io = new Server(server);
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+
     console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+
+    socket.on('news', (msg) => {
+        socket.emit('news', {});
+    });
+
+    socket.on('marketEquity', (msg) => {
+        socket.emit('tradeEquity', {});
+    });
+
+    socket.on('marketBond', (msg) => {
+        socket.emit('marketBond', {});
+    });
+
 });
+
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
